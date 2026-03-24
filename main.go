118e77e634a1da89f00e69d5522116aafbda7f38
𝓑𝓛𝓐𝓧𝓚 𝓜𝓘𝓡𝓐 <blaxkmira128@gmail.com>
2026-03-25 02:15:23 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello Nigga from Go Web Server ")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
